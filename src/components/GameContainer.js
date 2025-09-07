386793aa3550f57c83876ec5e9cc2dd1a7d2815e
wealthymanx <130352804+wealthymanx@users.noreply.github.com>
2025-09-07 13:48:46 -0700

    Api Integration

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,13 +1,13 @@
 export default function GameContainer({ gameData }) {
     return (
         <div>
-         <h2>{gameData.title}</h2>
+         <h2>{gameData.name}</h2>
          <p>Platform: {gameData.platforms.join(', ')}</p>
          <p>Released: {gameData.releaseDate}</p>
          <p>Genre: {gameData.genre}</p>
          <p>Rating: {gameData.rating}</p>
          <p>Playtime: {gameData.playTime}</p>
-         <p></p>
+         <p>Description: {gameData.description}</p>
         </div>
     )
 }
