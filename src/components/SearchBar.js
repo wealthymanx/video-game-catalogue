594f39de84b6d22afdffb125a6a8918e58b2a1d6
wealthymanx <130352804+wealthymanx@users.noreply.github.com>
2025-10-06 13:18:11 -0700

    adds star ratings and %, cleans up game container

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,36 +6,39 @@ const SearchBar = ({ query, setQuery, games }) => {
        console.log(games, "games")
 
        const filteredGames = games?.filter(game =>
-        game.name.includes(query)
+        game.name.toLowerCase().startsWith(query.toLowerCase())
         
      );
 
     const firstGame = filteredGames?.[0]?.name
+    const secondGame = filteredGames?.[1]?.name
+    const thirdGame = filteredGames?.[2]?.name
 
-    console.log(filteredGames,"filtered")
+    // console.log(filteredGames,"filtered")
     
     const handleChange = (value) => {
+        // value.length > 0 ? setDropdown(true) : setDropdown(false);
+        setDropdown(value.length > 0);
         setQuery(value);
-        setDropdown(true);
-    }
-    console.log(handleChange,"handle change")
 
-    const handleClear = () => {
-        setDropdown('');
     }
+
     return (
         <div className="flex items-center relative">
             <input 
             type="search" 
             placeholder="Search games" 
             aria-label="Search"
-            className="px-6 py-1 text-gray-700 rounded-lg border-4 border-black"
+            className="px-3 py-1 text-black rounded-lg border-4 border-black focus:outline-none focus:border-4 focus:border-black"
             value={query}
             onChange={(e) => handleChange(e.target.value)}  
             />
             <button className="p-2"> <FaSearch className= "w-6 h-6" /></button>
            <div className="absolute top-full left-0 mt-1">
-            {dropDown && <div className="border rounded bg-white z-30 text-black">{firstGame}</div>}
+            {dropDown && <div className="border rounded bg-white z-30 text-black">{firstGame}<br />
+                {secondGame}<br />
+                {thirdGame}<br />
+            </div>}
                 
         </div> 
         </div>
